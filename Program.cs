2c381c1fc338a0a4a4b94f93bcd38cbd9300285e
davitglonti <datiglonti@gmail.com>
2023-04-21 21:33:08 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// first project
+using System;
+
+public class User
+{
+    public static void Main(string[] args)
+    {
+        string expectedUsername = "myusername";
+        string expectedPassword = "mypassword";
+        int maxTries = 5;
+        int tries = 0;
+        while(tries< maxTries) {
+            Console.Write("Enter username:");
+            var username = Console.ReadLine();
+            Console.Write("Enter Password: ");
+            var password = Console.ReadLine();
+
+            if(username == expectedUsername && password == expectedPassword ) {
+                Console.WriteLine("Success");
+                break;
+            }else {
+                 Console.WriteLine("inCorrect");
+                 tries++;
+            }
+        }
+
+        if(tries==maxTries){
+            Console.WriteLine("max attempts reached");
+        }
+
+    }
+}
